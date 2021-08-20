4b653fb17d15ad1e57ec533b9daa36eea81639c7
vinaypuppal <me@vinaypuppal.com>
2021-08-20 13:03:49 +0530

    accept cli args and write to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let mut arguments = std::env::args().skip(1);
+    let key = arguments.next().expect("Needs key");
+    let value = arguments.next().expect("Needs value");
+    let contents = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", contents).expect("Failed to write to DB");
 }
